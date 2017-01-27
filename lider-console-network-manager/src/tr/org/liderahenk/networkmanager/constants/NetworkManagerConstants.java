5287af5155c81a0be3657aa84eb08906b21321ae
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-01-27 09:15:03 +0300

    fix 1.0.0-beta version

--- lider-console-network-manager/src/tr/org/liderahenk/networkmanager/constants/NetworkManagerConstants.java
+++ lider-console-network-manager/src/tr/org/liderahenk/networkmanager/constants/NetworkManagerConstants.java
@@ -4,7 +4,7 @@ public class NetworkManagerConstants {
 	
 	public static final String PLUGIN_NAME = "network-manager";
 	
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-beta";
 	
 	public static final class PARAMETERS {
 		public static final String IP = "ip";
